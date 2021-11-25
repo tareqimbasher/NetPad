6c00d85d6255ee2def33b7d93b552ad27fe908b9
Tareq Imbasher <tareq@meccasoft.com>
2021-11-25 03:21:45 +0200

    Delete Domain.TextEditing folder, it is no longer valid

--- src/Apps/NetPad.Avalonia/App.axaml.cs
+++ src/Apps/NetPad.Avalonia/App.axaml.cs
@@ -1,27 +1,16 @@
-using System.Linq;
-using System.Reflection;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NetPad.Scripts;
-using NetPad.Sessions;
-using NetPad.TextEditing;
-using NetPad.TextEditing.OmniSharp;
 using NetPad.ViewModels;
 using NetPad.Views;
-using ReactiveUI;
 using Splat;
-using Splat.Microsoft.Extensions.DependencyInjection;
 
 namespace NetPad
 {
     public class App : Application
     {
         private Startup _startup;
-        
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -39,7 +28,7 @@ namespace NetPad
                     DataContext = Locator.Current.GetRequiredService<MainWindowViewModel>()
                 };
             }
-            
+
             base.OnFrameworkInitializationCompleted();
         }
     }
