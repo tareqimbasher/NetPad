b4775efe7d5585b2bd43717c0bf54eae66e65154
Tareq Imbasher <tareq@meccasoft.com>
2021-11-26 15:44:44 +0200

    Add colored console logging provider to Aurelia app

--- src/Apps/NetPad.Web/App/src/main.ts
+++ src/Apps/NetPad.Web/App/src/main.ts
@@ -1,4 +1,4 @@
-import Aurelia, {Registration} from 'aurelia';
+import Aurelia, {ColorOptions, ConsoleSink, LoggerConfiguration, LogLevel, Registration} from 'aurelia';
 // import "bootstrap";
 // import 'bootstrap/dist/js/bootstrap.bundle';
 import './styles/main.scss';
@@ -7,6 +7,11 @@ import 'bootstrap-icons/font/bootstrap-icons.scss';
 const startupOptions = new URLSearchParams(window.location.search);
 
 const app = Aurelia.register(
+    LoggerConfiguration.create({
+        colorOptions: ColorOptions.colors,
+        level: LogLevel.trace,
+        sinks: [ConsoleSink],
+    }),
     Registration.instance(URLSearchParams, startupOptions),
     Registration.instance(String, window.location.origin),
 );
