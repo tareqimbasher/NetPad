0064630a7f68039f48cbbffd0717f8ded1ea4f4b
Tareq Imbasher <tareq@meccasoft.com>
2021-12-31 01:46:16 +0200

    Adds appsettings.Local.json as an option configuration source

--- src/Apps/NetPad.Web/Program.cs
+++ src/Apps/NetPad.Web/Program.cs
@@ -1,5 +1,6 @@
 using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace NetPad
@@ -13,6 +14,10 @@ namespace NetPad
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.Local.json", optional: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseElectron(args);
