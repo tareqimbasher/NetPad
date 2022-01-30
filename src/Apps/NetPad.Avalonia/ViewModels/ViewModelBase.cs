871600db7d8f08ed94f49e6c04f94520cf44e547
Tareq Imbasher <tareq@meccasoft.com>
2022-01-30 21:52:09 +0200

    Delete obsolete Avalonia WPF app

--- src/Apps/NetPad.Avalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using ReactiveUI;
-
-namespace NetPad.ViewModels
-{
-    public abstract class ViewModelBase : ReactiveObject, IActivatableViewModel
-    {
-        public ViewModelActivator Activator { get; } = new ViewModelActivator();
-    }
-}
