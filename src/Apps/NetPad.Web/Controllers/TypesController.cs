95299202332dba6cef48601ca574c5c79457eeca
Tareq Imbasher <tareq@meccasoft.com>
2022-01-31 00:22:59 +0200

    Change how additional types are exposed. This new way doesn't generate a long TypeScript type name that represents the tuple that we used to use

--- src/Apps/NetPad.Web/Controllers/TypesController.cs
+++ src/Apps/NetPad.Web/Controllers/TypesController.cs
@@ -8,17 +8,26 @@ namespace NetPad.Controllers
     [Route("types")]
     public class TypesController : Controller
     {
+        // This endpoint was created for the sole reason of providing Swagger with types that are not exposed by other endpoints
+        // that we want in the generated code
+        [ProducesResponseType(typeof(Types), 200)]
         [HttpGet]
-        public (
-            SettingsUpdated,
-            Script,
-            ScriptPropertyChanged,
-            ScriptConfigPropertyChanged,
-            ScriptOutputEmitted,
-            EnvironmentsAdded,
-            EnvironmentsRemoved,
-            EnvironmentPropertyChanged,
-            ActiveEnvironmentChanged
-        )? AdditionalTypes() => null;
+        public void AdditionalTypes()
+        {
+        }
+
+        class Types
+        {
+            public Script Script { get; set; }
+            public SettingsUpdated SettingsUpdated { get; set; }
+            public ScriptPropertyChanged ScriptPropertyChanged { get; set; }
+            public ScriptConfigPropertyChanged ScriptConfigPropertyChanged { get; set; }
+            public ScriptOutputEmitted ScriptOutputEmitted { get; set; }
+            public EnvironmentsAdded EnvironmentsAdded { get; set; }
+            public EnvironmentsRemoved EnvironmentsRemoved { get; set; }
+            public EnvironmentPropertyChanged EnvironmentPropertyChanged { get; set; }
+            public ActiveEnvironmentChanged ActiveEnvironmentChanged { get; set; }
+            public ScriptDirectoryChanged ScriptDirectoryChanged { get; set; }
+        }
     }
 }
