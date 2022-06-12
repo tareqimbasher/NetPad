0af397d0e4da57c319f0370cdae12a23541ece43
Tareq Imbasher <tareq@meccasoft.com>
2022-06-13 02:25:49 +0300

    Adds documentation and an unregister method to the shortcut manager

--- src/Apps/NetPad.Apps.App/App/src/core/@domain/shortcuts/shortcut-manager.ts
+++ src/Apps/NetPad.Apps.App/App/src/core/@domain/shortcuts/shortcut-manager.ts
@@ -1,21 +1,50 @@
-import {DI, IContainer} from "aurelia";
+import {DI, IContainer, ILogger} from "aurelia";
 import {Shortcut} from "./shortcut";
 import {ShortcutActionExecutionContext} from "./shortcut-action-execution-context";
 import {IEventBus} from "@domain";
 
 export interface IShortcutManager {
+    /**
+     * Initializes the ShortcutManager and starts listening for keyboard events.
+     */
     initialize(): void;
+
+    /**
+     * Adds a shortcut to the shortcut registry.
+     * @param shortcut The shortcut to register.
+     */
     registerShortcut(shortcut: Shortcut): void;
+
+    /**
+     * Removes a shortcut from the shortcut registry.
+     * @param shortcut The shortcut to unregister.
+     */
+    unregisterShortcut(shortcut: Shortcut): void;
+
+    /**
+     * Finds a shortcut by its name, if one exists.
+     * @param name The name of the shortcut to get.
+     */
     getShortcutByName(name: string): Shortcut | undefined;
+
+    /**
+     * Executes a shortcut.
+     * @param shortcut
+     */
     executeShortcut(shortcut: Shortcut): void;
 }
 
 export const IShortcutManager = DI.createInterface<IShortcutManager>();
 
 export class ShortcutManager implements IShortcutManager{
-    private registry: Shortcut[] = [];
+    private readonly registry: Shortcut[] = [];
+    private readonly logger: ILogger;
 
-    constructor(@IEventBus readonly eventBus: IEventBus, @IContainer readonly container: IContainer) {
+    constructor(
+        @IEventBus private readonly eventBus: IEventBus,
+        @IContainer private readonly container: IContainer,
+        @ILogger logger: ILogger) {
+        this.logger = logger.scopeTo(nameof(ShortcutManager));
     }
 
     public getShortcutByName(name: string): Shortcut | undefined {
@@ -23,7 +52,10 @@ export class ShortcutManager implements IShortcutManager{
     }
 
     public registerShortcut(shortcut: Shortcut) {
+        this.logger.debug(`Registering shortcut "${shortcut.name}"`);
+
         const existing = this.registry.findIndex((s) => s.matches(shortcut));
+
         if (existing >= 0) {
             this.registry[existing] = shortcut;
         } else {
@@ -31,7 +63,19 @@ export class ShortcutManager implements IShortcutManager{
         }
     }
 
+    public unregisterShortcut(shortcut: Shortcut) {
+        this.logger.debug(`Unregistering shortcut "${shortcut.name}"`);
+
+        const ix = this.registry.indexOf(shortcut);
+
+        if (ix >= 0) {
+            this.registry.splice(ix, 1);
+        }
+    }
+
     public initialize() {
+        this.logger.debug("Initializing");
+
         document.addEventListener("keydown", (ev) => {
             const shortcut = this.registry.find((s) => s.isEnabled && s.matches(ev));
             if (!shortcut) return;
@@ -43,6 +87,8 @@ export class ShortcutManager implements IShortcutManager{
     }
 
     public executeShortcut(shortcut: Shortcut) {
+        this.logger.debug(`Executing shortcut "${shortcut.name}"`);
+
         if (shortcut.action) {
             const context = new ShortcutActionExecutionContext(this.container);
             shortcut.action(context);
