tareqimbasher/NetPad

commit a0edc2bfa753e629c98a2bc6d1327f6a720a0a6b

fix: Replace greater than and less than symbols with their HTML representations to fix strings that contain them not showing in HTML viewers

src/External/O2Html/Converters/DotNetTypeWithStringRepresentationHtmlConverter.cs
@@ -13,7 +13,10 @@ public class DotNetTypeWithStringRepresentationHtmlConverter : HtmlConverter
 
         var str = obj.ToString()?
             .Replace("\n", "<br/>")
-            .Replace(" ", "&nbsp;");
+            .Replace(" ", "&nbsp;")
+            .Replace("<", "&lt;")
+            .Replace(">", "&gt;")
+        ;
 
         return new Element("span").WithText(str ?? string.Empty);
     }

commit 38f9e1b83c50d1d38351c8a9292eb94e8f7a20f6

Fixes a bug introduced in a0edc2bf where new lines were rendered as "<br/>" in the results view instead of rendering a new line

src/External/O2Html/Converters/DotNetTypeWithStringRepresentationHtmlConverter.cs
@@ -12,10 +12,10 @@ public class DotNetTypeWithStringRepresentationHtmlConverter : HtmlConverter
             return new Null().WithAddClass(htmlSerializer.SerializerSettings.CssClasses.Null);
 
         var str = obj.ToString()?
-            .Replace("\n", "<br/>")
             .Replace(" ", "&nbsp;")
             .Replace("<", "&lt;")
             .Replace(">", "&gt;")
+            .Replace("\n", "<br/>")
         ;
 
         return new Element("span").WithText(str ?? string.Empty);