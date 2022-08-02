66419f7351f3096cd079e8e65059c3215ac58ee3
Tareq Imbasher <tareq@meccasoft.com>
2022-08-03 01:34:32 +0300

    Some cleanup, xml docs and removal of some obsolete code

--- src/Apps/NetPad.Apps.App/App/src/core/@common/index.ts
+++ src/Apps/NetPad.Apps.App/App/src/core/@common/index.ts
@@ -3,7 +3,6 @@ export * from "./background-services/ibackground-service";
 export * from "./events/subscription-token";
 
 export * from "./utils/key-codes";
-export * from "./utils/mapper";
 export * from "./utils/util";
 export * from "./utils/system";
 export * from "./utils/mutation-observer";
