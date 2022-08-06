7466f2249990a8b2a19c5a7326a731c1b4d23827
Tareq Imbasher <tareq@meccasoft.com>
2022-08-06 16:38:47 +0300

    OmniSharp.NET: ability to subscribe to STDIO events produced by OmniSharp server

--- src/External/OmniSharp.NET/Stdio/IOmniSharpStdioServer.cs
+++ src/External/OmniSharp.NET/Stdio/IOmniSharpStdioServer.cs
@@ -1,10 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+using OmniSharp.Events;
 
 namespace OmniSharp.Stdio
 {
     public interface IOmniSharpStdioServer : IOmniSharpServer
     {
+        /// <summary>
+        /// Subscribe to an event.
+        /// </summary>
+        /// <param name="eventType">The event type. See <see cref="OmniSharp.Models.Events.EventTypes"/> for a list of event types.</param>
+        /// <param name="handler">A delegate to handle the event. The delegate will be passed the entire event, not just the body.</param>
+        SubscriptionToken SubscribeToEvent(string eventType, Action<JsonNode> handler);
     }
 }
