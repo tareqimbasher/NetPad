d191d2502369cb3f6f79058a524b9dbb38cd8f08
Tareq Imbasher <tareq@meccasoft.com>
2022-09-24 01:32:10 +0300

    Refresh data connection when user updates it

--- src/Core/NetPad.Application/CQs/SaveDataConnectionCommand.cs
+++ src/Core/NetPad.Application/CQs/SaveDataConnectionCommand.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using MediatR;
 using NetPad.Data;
 using NetPad.Events;
@@ -6,7 +7,6 @@ namespace NetPad.CQs;
 
 public class SaveDataConnectionCommand : Command
 {
-
     public SaveDataConnectionCommand(DataConnection connection)
     {
         Connection = connection;
@@ -17,21 +17,66 @@ public class SaveDataConnectionCommand : Command
     public class Handler : IRequestHandler<SaveDataConnectionCommand, Unit>
     {
         private readonly IDataConnectionRepository _dataConnectionRepository;
+        private readonly IMediator _mediator;
         private readonly IEventBus _eventBus;
 
-        public Handler(IDataConnectionRepository dataConnectionRepository, IEventBus eventBus)
+        public Handler(IDataConnectionRepository dataConnectionRepository, IMediator mediator, IEventBus eventBus)
         {
             _dataConnectionRepository = dataConnectionRepository;
+            _mediator = mediator;
             _eventBus = eventBus;
         }
 
         public async Task<Unit> Handle(SaveDataConnectionCommand request, CancellationToken cancellationToken)
         {
-            await _dataConnectionRepository.SaveAsync(request.Connection);
+            var updated = request.Connection;
+            var existing = await _dataConnectionRepository.GetAsync(request.Connection.Id);
 
-            await _eventBus.PublishAsync(new DataConnectionSavedEvent(request.Connection));
+            await _dataConnectionRepository.SaveAsync(updated);
+
+            await _eventBus.PublishAsync(new DataConnectionSavedEvent(updated));
+
+            bool shouldRefreshResources = existing == null || HasChangedOtherThanName(existing, updated);
+
+            if (shouldRefreshResources)
+            {
+                // We don't want to wait for this
+                _ = _mediator.Send(new RefreshDataConnectionCommand(updated.Id));
+            }
 
             return Unit.Value;
+        }
+
+        private bool HasChangedOtherThanName(DataConnection existing, DataConnection updated)
+        {
+            var existingConnectionType = existing.GetType();
+            var updatedConnectionType = updated.GetType();
+
+            if (existingConnectionType != updatedConnectionType)
+            {
+                return true;
+            }
+
+            var properties = updatedConnectionType
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.Name != nameof(DataConnection.Name));
+
+            foreach (var property in properties)
+            {
+                var existingConnectionValue = property.GetValue(existing);
+                var updatedConnectionValue = property.GetValue(updated);
+
+                bool changed = (existingConnectionValue != null && updatedConnectionValue == null)
+                               || (existingConnectionValue == null && updatedConnectionValue != null)
+                               || existingConnectionValue?.Equals(updatedConnectionValue) != true;
+
+                if (changed)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
