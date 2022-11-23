adebb51341bfedcf286e7b73decf8ae73cbff95c
Tareq Imbasher <tareq@meccasoft.com>
2022-11-23 17:23:32 +0200

    Fix output view duplicate icons showing

--- src/Apps/NetPad.Apps.App/App/src/windows/main/script-environments/script-environment-view/output-view/output-view.ts
+++ src/Apps/NetPad.Apps.App/App/src/windows/main/script-environments/script-environment-view/output-view/output-view.ts
@@ -3,6 +3,6 @@ import {ScriptEnvironment} from "@domain";
 
 export class OutputView {
     @bindable public environment: ScriptEnvironment;
-    @bindable public onCloseRequested: () => void;
+    @bindable public close: () => void;
     public view: string = "Results";
 }
