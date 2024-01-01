94e392bb2ecaf0deda04b3b432c2dc2d43e1a257
Tareq Imbasher <tareq@meccasoft.com>
2024-01-01 17:25:17 +0200

    TMP

--- src/Core/NetPad.Domain/Utilities/DelegateUtil.cs
+++ src/Core/NetPad.Domain/Utilities/DelegateUtil.cs
@@ -6,16 +6,48 @@ namespace NetPad.Utilities;
 
 public static class DelegateUtil
 {
-    public static Action<T> Debounce<T>(this Action<T> func, int milliseconds = 300)
+    public static Action Debounce(this Action func, int milliseconds = 300, bool immediate = false)
     {
         CancellationTokenSource? cancelTokenSource = null;
+        Task? task = null;
+
+        return () =>
+        {
+            cancelTokenSource?.Cancel();
+            cancelTokenSource = new CancellationTokenSource();
+
+            if (immediate && (task == null || task.IsCompleted))
+            {
+                func();
+            }
+
+            task = Task.Delay(milliseconds, cancelTokenSource.Token)
+                .ContinueWith(t =>
+                {
+                    if (t.IsCompletedSuccessfully)
+                    {
+                        func();
+                    }
+                }, TaskScheduler.Default);
+        };
+    }
+
+    public static Action<T> Debounce<T>(this Action<T> func, int milliseconds = 300, bool immediate = false)
+    {
+        CancellationTokenSource? cancelTokenSource = null;
+        Task? task = null;
 
         return arg =>
         {
             cancelTokenSource?.Cancel();
             cancelTokenSource = new CancellationTokenSource();
 
-            Task.Delay(milliseconds, cancelTokenSource.Token)
+            if (immediate && (task == null || task.IsCompleted))
+            {
+                func(arg);
+            }
+
+            task = Task.Delay(milliseconds, cancelTokenSource.Token)
                 .ContinueWith(t =>
                 {
                     if (t.IsCompletedSuccessfully)
