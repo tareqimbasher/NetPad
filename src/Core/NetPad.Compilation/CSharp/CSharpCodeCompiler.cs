tareqimbasher/NetPad

commit 8b6927acd5846d8ada1d18dd3a55520f7dd90f25

#157 Use a fixed name for compiled script assemblies

src/Core/NetPad.Compilation/CSharp/CSharpCodeCompiler.cs
@@ -23,16 +23,14 @@ public class CSharpCodeCompiler : ICodeCompiler
 
     public CompilationResult Compile(CompilationInput input)
     {
-        string assemblyName = "NetPad_CompiledAssembly";
+        string assemblyName = "NetPadScript";
 
         if (input.OutputAssemblyNameTag != null)
         {
             assemblyName += $"_{input.OutputAssemblyNameTag}";
         }
 
-        assemblyName += Guid.NewGuid().ToString();
-
-        string assemblyFileName = assemblyName + GetCompiledFileExtension(input.OutputKind);
+        string assemblyFileName = $"{assemblyName}{GetCompiledFileExtension(input.OutputKind)}";
 
         var compilation = CreateCompilation(input, assemblyName);
 

src/Infrastructure/NetPad.Infrastructure/Data/EntityFrameworkCore/EntityFrameworkResourcesGenerator.cs
@@ -130,7 +130,6 @@ public class EntityFrameworkResourcesGenerator : IDataConnectionResourcesGenerat
                 null,
                 assemblyFileReferences)
             .WithOutputKind(OutputKind.DynamicallyLinkedLibrary)
-            .WithOutputAssemblyNameTag($"data-connection_{efConnection.Id}")
         );
     }
 

src/Apps/NetPad.Apps.App/BackgroundServices/DebugAssemblyUnloadBackgroundService.cs
@@ -8,7 +8,7 @@ namespace NetPad.BackgroundServices;
 
 /// <summary>
 /// Periodically outputs the compiled script assemblies loaded by the program into memory. This is
-/// mainly used to debug assembly unloading after script execution.
+/// mainly used to debug assembly unloading after script execution (when using InMemoryScriptRuntime only).
 /// </summary>
 public class DebugAssemblyUnloadBackgroundService : BackgroundService
 {
@@ -26,7 +26,7 @@ public class DebugAssemblyUnloadBackgroundService : BackgroundService
 
                 int count = 0;
                 var names = AppDomain.CurrentDomain.GetAssemblies().Select(a => a.FullName)
-                    .Where(n => n?.Contains("NetPad_CompiledAssembly") == true)
+                    .Where(n => n?.Contains("NetPadScript") == true)
                     .Select(s => $"{++count}. {s?.Split(',')[0]}");
 
                 _logger.LogDebug("Loaded NetPad script assemblies (count: {Count}):\n{Assemblies}",

src/Core/ScriptRuntimes/NetPad.ExternalProcessScriptRuntime/ExternalProcessScriptRuntime.Setup.cs
@@ -128,7 +128,6 @@ public partial class ExternalProcessScriptRuntime
                     _script.Config.TargetFrameworkVersion,
                     referenceAssemblyImages.Select(a => a.Image).ToHashSet(),
                     referenceAssemblyPaths)
-                .WithOutputAssemblyNameTag(_script.Name)
                 .WithOptimizationLevel(_script.Config.OptimizationLevel)
                 .WithUseAspNet(_script.Config.UseAspNet);
 

src/Core/ScriptRuntimes/NetPad.InMemoryScriptRuntime/InMemoryScriptRuntime.cs
@@ -175,8 +175,7 @@ public sealed class InMemoryScriptRuntime : IScriptRuntime
                 fullProgram,
                 _script.Config.TargetFrameworkVersion,
                 referenceAssemblyImages.Select(a => a.Image).ToHashSet(),
-                referenceAssemblyPaths)
-            .WithOutputAssemblyNameTag(_script.Name));
+                referenceAssemblyPaths));
 
         if (!compilationResult.Success)
         {

commit d2c383bb5d151c3e6b106ebd290ee8d78bd8be3b

Fixes regression caused by 8b6927ac
Script fails to execute with System.MissingMethodException: Entry point not found in assembly NetPadScript when a data connection is assigned to script.

The issue is caused by the compiled data connection assembly having the same name as the compiled script assembly, so the data connection assembly overwrites the script assembly causing the issue.

src/Core/NetPad.Compilation/CSharp/CSharpCodeCompiler.cs
@@ -23,12 +23,7 @@ public class CSharpCodeCompiler : ICodeCompiler
 
     public CompilationResult Compile(CompilationInput input)
     {
-        string assemblyName = "NetPadScript";
-
-        if (input.OutputAssemblyNameTag != null)
-        {
-            assemblyName += $"_{input.OutputAssemblyNameTag}";
-        }
+        string assemblyName = !string.IsNullOrWhiteSpace(input.AssemblyName) ? input.AssemblyName : "NetPadScript";
 
         string assemblyFileName = $"{assemblyName}{GetCompiledFileExtension(input.OutputKind)}";
 

src/Core/NetPad.Compilation/CompilationInput.cs
@@ -30,7 +30,7 @@ public class CompilationInput
     public OutputKind OutputKind { get; private set; }
     public DotNetFrameworkVersion TargetFrameworkVersion { get; private set; }
     public string Code { get; }
-    public string? OutputAssemblyNameTag { get; private set; }
+    public string? AssemblyName { get; private set; }
     public OptimizationLevel OptimizationLevel { get; private set; }
     public bool UseAspNet { get; private set; }
     public HashSet<byte[]> AssemblyImageReferences { get; }
@@ -42,9 +42,9 @@ public class CompilationInput
         return this;
     }
 
-    public CompilationInput WithOutputAssemblyNameTag(string? outputAssemblyNameTag)
+    public CompilationInput WithAssemblyName(string? assemblyName)
     {
-        OutputAssemblyNameTag = outputAssemblyNameTag;
+        AssemblyName = assemblyName;
         return this;
     }
 

src/Infrastructure/NetPad.Infrastructure/Data/EntityFrameworkCore/EntityFrameworkResourcesGenerator.cs
@@ -130,6 +130,7 @@ public class EntityFrameworkResourcesGenerator : IDataConnectionResourcesGenerat
                 null,
                 assemblyFileReferences)
             .WithOutputKind(OutputKind.DynamicallyLinkedLibrary)
+            .WithAssemblyName($"DataConnection_{efConnection.Id}")
         );
     }
 