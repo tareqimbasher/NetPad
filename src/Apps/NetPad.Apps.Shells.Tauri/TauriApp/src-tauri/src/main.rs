08b2357e995fd9ecdcf9d69716e5df19c2c1ab87
Tareq Imbasher <tareq@meccasoft.com>
2024-09-30 00:21:11 +0300

    Revert "Disable DMABUF rendering on linux systems to resolve upstream DMABUF rendering issue related to NVIDIA graphics"
    This reverts commit 9dedf933c32e2eccdc0297bd55cc944514dce564.

--- src/Apps/NetPad.Apps.Shells.Tauri/TauriApp/src-tauri/src/main.rs
+++ src/Apps/NetPad.Apps.Shells.Tauri/TauriApp/src-tauri/src/main.rs
@@ -2,19 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    // Setting this env var resolves an issue on some linux setups with NVIDIA graphics
-    // where the app launches with an empty screen and the error:
-    //      Failed to create GBM buffer of size {win_width}x{win_height}: Invalid argument
-    //
-    // TODO only disable on systems with NVIDIA graphics
-    #[cfg(any(
-        target_os = "linux",
-        target_os = "freebsd",
-        target_os = "dragonfly",
-        target_os = "openbsd",
-        target_os = "netbsd"
-    ))]
-    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-
     app_lib::run();
 }
