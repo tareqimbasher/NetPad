603b161136f4c0add6f90775efb1726acea06818
Tareq Imbasher <tareq@meccasoft.com>
2025-05-01 02:30:48 +0300

    Version v0.9.0

--- src/Apps/NetPad.Apps.App/electron.manifest.js
+++ src/Apps/NetPad.Apps.App/electron.manifest.js
@@ -6,7 +6,7 @@ const electronBuilderConfig = {
     appId: "com.netpad.app",
     productName: "NetPad",
     copyright: "Copyright © 2021 Tareq Imbasher",
-    buildVersion: "0.8.0",
+    buildVersion: "0.9.0",
     compression: "maximum",
     directories: {
         output: "../../../bin/Desktop"
