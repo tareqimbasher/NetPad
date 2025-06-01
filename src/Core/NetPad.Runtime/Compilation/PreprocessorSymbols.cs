2593d24d53b4ee9acd6d3c185872fbe6943c2f81
jesterret <jesterret@users.noreply.github.com>
2025-06-01 12:52:58 +0200

    Fix OR_GREATER symbols

--- src/Core/NetPad.Runtime/Compilation/PreprocessorSymbols.cs
+++ src/Core/NetPad.Runtime/Compilation/PreprocessorSymbols.cs
@@ -13,8 +13,22 @@ public static class PreprocessorSymbols
 
     public static string[] For(DotNetFrameworkVersion version)
     {
-        var ver = version.GetMajorVersion();
-        return ["NET", $"NET{ver}_0", $"NET{ver}_0_OR_GREATER"];
+        return ForInternal(version).ToArray();
+
+        IEnumerable<string> ForInternal(DotNetFrameworkVersion version)
+        {
+            var ver = version.GetMajorVersion();
+
+            yield return "NET";
+            yield return  $"NET{ver}_0";
+
+            // Add all past versions symbols
+            while (ver - 4 > 0)
+            {
+                yield return $"NET{ver}_0_OR_GREATER";
+                ver--;
+            }
+        }
     }
 
     public static string[] For(OptimizationLevel optimizationLevel, DotNetFrameworkVersion version) => For(optimizationLevel).Union(For(version)).ToArray();
